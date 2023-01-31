75ecb1860b0a16d167f3a92b4c191acbed2c40df
gidskql6671 <gidskql6671@gmail.com>
2023-01-31 20:46:02 +0900

    웹서버

--- chapter18/chat_serv.cpp
+++ chapter18/chat_serv.cpp
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 {
 	int serv_sock, clnt_sock;
 	struct sockaddr_in serv_adr, clnt_adr;
-	int clnt_adr_sz;
+	socklen_t clnt_adr_sz;
 	pthread_t t_id;
 	if(argc!=2) {
 		printf("Usage : %s <port>\n", argv[0]);
